6bcc5dd1829eb483724fa5e053c759c087900e8f
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-06-29 14:50:24 -0500

    :sparkles: Reusable component (in separate Vue instances)

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-var one = new Vue({
-  el: "#vue-app-one",
-  data: {
-    title: "Vue Title One"
+Vue.component("greeting", {
+  template:
+    "<p>Hey there, I am {{name}}. <button v-on:click='changeName'>Change name</button></p>",
+  data: function() {
+    return {
+      name: "Yoshi"
+    };
   },
-  methods: {},
-  computed: {
-    greet: function() {
-      return "Hello from app one!";
-    }
-  }
-});
-
-var two = new Vue({
-  el: "#vue-app-two",
-  data: { title: "Vue Title Two" },
   methods: {
-    changeTitle: function() {
-      one.title = "Title changed!";
-    }
-  },
-  computed: {
-    greet: function() {
-      return "Hello from app two!";
+    changeName: function() {
+      this.name = "Mario";
     }
   }
 });
 
-// two.title = "Changed from outside";
+new Vue({
+  el: "#vue-app-one"
+});
+
+new Vue({
+  el: "#vue-app-two"
+});
