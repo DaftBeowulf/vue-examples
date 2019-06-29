4ac5bae926013d3cfd08b5b21abdfe81e0f26225
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-06-29 13:48:11 -0500

    :sparkles: Computed properties

--- app.js
+++ app.js
@@ -7,24 +7,40 @@ new Vue({
     websiteTag: '<a href="https://www.astros.com">Astros</a>',
     age: 30,
     x: 0,
-    y: 0
+    y: 0,
+    logAge: "",
+    a: 0,
+    b: 0
   },
-  methods: {
-    greet: function(time) {
-      return `Good ${time}, ${this.name}!`;
+  //   methods: {
+  //     greet: function(time) {
+  //       return `Good ${time}, ${this.name}!`;
+  //     },
+  //     add: function(inc) {
+  //       this.age += inc;
+  //     },
+  //     subtract: function(dec) {
+  //       this.age -= dec;
+  //     },
+  //     updateXY: function(e) {
+  //       this.x = e.offsetX;
+  //       this.y = e.offsetY;
+  //     },
+  //     click: function() {
+  //       alert("Has been clicked!");
+  //     },
+  //     logName: function() {
+  //       console.log("Name entered");
+  //     }
+  //   },
+  computed: {
+    addToA: function() {
+      console.log("'A' has been clicked!");
+      return this.a + this.age;
     },
-    add: function(inc) {
-      this.age += inc;
-    },
-    subtract: function(dec) {
-      this.age -= dec;
-    },
-    updateXY: function(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-    },
-    click: function() {
-      alert("It's been clicked!");
+    addToB: function() {
+      console.log("'B' has been clicked!");
+      return this.b + this.age;
     }
   }
 });
